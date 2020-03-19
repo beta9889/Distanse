dc59d68d4588c368f620e727a605374eb292d082
beta9889 <beviseta@gmail.com>
2020-03-19 12:30:45 +0100

    inital commit

--- Tgui.cpp
+++ Tgui.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 #include <TGUI/TGUI.hpp>
 #include <fstream>
-void LoadWidgets( tgui::Gui& gui){
+void Startpage(tgui::Gui& gui){
+    tgui::Button::Ptr New = tgui::Button::create("New Character");
+    New-> setSize("25%", "25%");
+    New-> setPosition("25%", "50%");
+    gui.add(New, "new");
+}
+void NewStats(tgui::Gui& gui){
     
+    tgui::Button::Ptr Submit = tgui::Button::create("Submit");
+    Submit->setPosition("43%","60%");
+    Submit->setSize("10%", "5%");
+    gui.add(Submit); 
+
+    tgui::EditBox::Ptr name = tgui::EditBox::create();
+    name -> setDefaultText("Username");
+    name->setSize("20%" , "5%");
+    name->setPosition("40%","35%"); 
+    gui.add(name, "name");
+  
+    tgui::EditBox::Ptr strenght = tgui::EditBox::create();
+    strenght -> setDefaultText("Strenght");
+    strenght -> setSize("10%", "5%");
+    strenght->setPosition("45%", "40%");
+    gui.add(strenght, "str");
+
+    tgui::EditBox::Ptr dexterity = tgui::EditBox::create();
+    dexterity-> setDefaultText("Dexterity");
+    dexterity-> setSize("10%", "5%");
+    dexterity->setPosition("45%","45%");
+    gui.add(dexterity, "dex");
 
 }
 int main()
 {
     sf::RenderWindow window{{800, 600}, "Window"};
     tgui::Gui gui{window}; // Create the gui and attach it to the window
-    tgui::Button::Ptr button = tgui::Button::create("Submit");
-    tgui::EditBox::Ptr editbox = tgui::EditBox::create();
-    gui.add(button);
-    gui.add(editbox, "name");
-    button->setPosition("45%","60%");
-    editbox->setPosition("40%","35%"); 
-    button->setSize("10%", "5%");
-    editbox->setSize("15%" , "10%");
-   
+    
+    try{
+    Startpage(gui); 
+    }
+    catch (const tgui::Exception& e){
+	    std::cerr << "something went wrong :" << e.what() << std::endl;
+    }
+
 
     while (window.isOpen())
     {
@@ -27,8 +54,12 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
             else if (event.type == sf::Event::Resized){
-		window.setView(sf::View(sf::FloatRect(0.f, 0.f, static_cast<float>(event.size.width),static_cast<float>(event.size.height))));
+
+		window.setView(sf::View(sf::FloatRect(0.f, 0.f, static_cast<float>(event.size.width),
+						                static_cast<float>(event.size.height))));
 		gui.setView(window.getView());
+	
+	    //if (event.type ==sf
 	    }
             gui.handleEvent(event); // Pass the event to the widgets
         }
