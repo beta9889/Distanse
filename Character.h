a1f01c65008cddc4e69ccee96ca270d280623dad
beviseta@gmail.com <beviseta@gmail.com>
2020-05-07 13:51:23 +0200

    klass flytt

--- Character.h
+++ Character.h
@@ -1,14 +1,17 @@
-#ifndef Character
-#define Character
+
 #include <iostream>
 #include <string>
-class Character{
-public:
+#ifndef Character_H
+#define Character_H
+
+class Character
+{
+public :
     string name;
     void setstats(string chara, int str, int dex);
     void changehealth(int dmg, tgui::Gui& gui);
     void showstats();
-private:
+private :
     int health;
     int strenght;
     int dexterity;
